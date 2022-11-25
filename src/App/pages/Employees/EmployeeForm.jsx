245db6558cd368ba096dd04091f927243ad96f4e
HammadSiddique <hammad.siddique01@gmail.com>
2022-11-25 21:35:53 +0500

    Design employee form

---
+++ src/App/pages/Employees/EmployeeForm.jsx
@@ -0,0 +1,42 @@
+import { Grid } from '@material-ui/core';
+import React from 'react';
+import {useForm, Form} from '../../../components/useForm';
+import Controls from '../../../components/controls/Controls';
+
+const genderItems = [
+  {id: 'male', title: 'Male'},
+  {id: 'female', title: 'Female'},
+  {id: 'other', title: 'Other'},
+]
+
+const initialFormValues = {
+  id: 0,
+  fullName: '',
+  email: '',
+  mobile: '',
+  city: '',
+  gender: 'male',
+  departmentId: '',
+  hireDate: new Date(),
+  isPermanent: false,
+}
+
+export default function EmployeesForm() {
+
+  const {values, setValues, handleInputChange} = useForm(initialFormValues);
+
+  return (
+    <Form>
+      <Grid container>
+        <Grid item xs={6}>
+          <Controls.Input name='fullName' label='Full Name' value={values.fullName} onChange={handleInputChange} />
+          <Controls.Input name='email' label='Email' value={values.email} onChange={handleInputChange} />
+        </Grid>
+        <Grid item xs={6}>
+          <Controls.RadioGroup label='Gender' name='gender' value={values.gender} onChange={handleInputChange} items={genderItems}/>
+
+          </Grid>
+      </Grid>
+    </Form>
+  )
+}
--- src/App/pages/Employees/Employees.jsx
+++ src/App/pages/Employees/Employees.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import PageHeader from '../../../components/PageHeader'
-import EmployeesForm from './EmployeesForm'
+import EmployeeForm from './EmployeeForm'
 import { Person } from '@material-ui/icons'
+import { makeStyles, Paper } from '@material-ui/core'
+
+const useStyles = makeStyles(theme => ({
+  pageContent:{
+    margin: theme.spacing(5),
+    padding: theme.spacing(3),
+  }
+}))
 
 export default function Employees() {
+
+  const classes = useStyles();
+
   return (
     <>
     <PageHeader title='Employees Data' subTitle='Find all employees details here' icon={<Person fontSize='large' />}/>
-    <EmployeesForm />
+    <Paper className={classes.pageContent}>
+      <EmployeeForm />
+    </Paper>
     </>
   )
 }
