8478097fdd3ecbc3f5090d7d19cb1cddda505df1
HammadSiddique <hammad.siddique01@gmail.com>
2022-12-02 01:00:03 +0500

    Populate table with data

--- src/App/pages/Employees/Employees.jsx
+++ src/App/pages/Employees/Employees.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, {useState} from 'react'
 import PageHeader from '../../../components/PageHeader'
 import EmployeeForm from './EmployeeForm'
 import { Person } from '@material-ui/icons'
-import { makeStyles, Paper } from '@material-ui/core'
+import { makeStyles, Paper, TableBody, TableCell, TableRow } from '@material-ui/core'
+import useTable from '../../../components/useTable'
+import { getAllEmployees } from '../../../services/employeeService'
 
 const useStyles = makeStyles(theme => ({
   pageContent:{
@@ -15,11 +17,28 @@ export default function Employees() {
 
   const classes = useStyles();
 
+  const [records, setRecords] = useState(getAllEmployees)
+  const {TblContainer} = useTable();
+
   return (
     <>
     <PageHeader title='Employees Data' subTitle='Find all employees details here' icon={<Person fontSize='large' />}/>
     <Paper className={classes.pageContent}>
       <EmployeeForm />
+      <TblContainer>
+        <TableBody>
+          {
+            records.map(employee => (
+              <TableRow key={employee.id}>
+                <TableCell>{employee.fullName}</TableCell>
+                <TableCell>{employee.email}</TableCell>
+                <TableCell>{employee.mobile}</TableCell>
+                <TableCell>{employee.department}</TableCell>
+              </TableRow>
+            ))
+          }
+        </TableBody>
+      </TblContainer>
     </Paper>
     </>
   )
